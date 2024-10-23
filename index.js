1e547927e7964765c110c4d73bd2642ee60cf42b
daishadavis <daishadavis84@gmail.com>
2024-10-23 17:38:27 -0400

    pokedex 2.0

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-async function fetchData() {
+// Variables
+const searchBtn = document.getElementById('search-btn');
+const inputField = document.getElementById('name-input');
+const nameScreen = document.getElementById('name-screen');
+const imageScreen = document.getElementById('main-screen');
+const aboutScreen = document.getElementById('about-screen');
+const typeScreen = document.getElementById('type-screen');
+const idScreen = document.getElementById('id-screen');
+const rightButton = document.querySelector('.right-nav-button');
+const leftButton = document.querySelector('.left-nav-button');
 
-    try {
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+let current = 1;
 
-        if(!response.ok) {
-            throw new Error("Could not fetch resource")
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker
+        .register('/serviceWorker.js')
+        .then((res) => console.log('service worker registered'))
+        .catch((err) => console.log('service worker not registered', err));
+    });
+  }
+  
+
+// Get pokemonData
+const getPokemonData = (pokemon) => {
+    if (!pokemon) {
+      return;
+    }
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.id) {
+          return;
         }
+        current = data.id;
+        let id = ('00' + data.id).slice(-3);
+        imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
+        nameScreen.innerHTML = data.name;
+        typeScreen.innerHTML = data.types[0].type.name;
+        idScreen.innerHTML = `#${data.id}`;
+        aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${data.weight / 10}kg`;
+        inputField.value = '';
+      });
+  };
 
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite")
 
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block";
+  // Create Event Listeners 
+  inputField.addEventListener('keydown', (event) => event.key === 'Enter' && searchBtn.click());
+  searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
+  rightButton.addEventListener('click', () => {
+    getPokemonData(current + 1);
+  });
+  leftButton.addEventListener('click', () => {
+    if (current <= 1) {
+      return;
     }
-    catch(error) {
-        console.error(error)
+    getPokemonData(current - 1);
+  });
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      rightButton.click();
+    } else if (event.key === 'ArrowLeft') {
+      leftButton.click();
     }
-}
+  });
